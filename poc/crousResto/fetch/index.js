51967911ed0db9db4bc885abadb74981a496b82e
Le_Roux-nard <65075968+Le-Roux-nard@users.noreply.github.com>
2022-02-05 12:55:32 -0500

    mise à jour POC #9
    mise à jour des liens de la POC suite à une modification dans les données fournies

--- poc/crousResto/fetch/index.js
+++ poc/crousResto/fetch/index.js
@@ -2,9 +2,10 @@ const fetch = require("node-fetch");
 const convert = require("xml-js");
 
 (async () => {
-    fetch("http://webservices-v2.crous-mobile.fr/feed/versailles/externe/resto.xml").then(async res =>
+    fetch("https://www.data.gouv.fr/fr/datasets/r/399ff888-42bf-4cf0-a562-c0987dd27d4e").then(async res =>
         JSON.parse(convert.xml2json(await res.text(), { compact: true, nativeType: true }))
     ).then(data => {
         console.log(data);
     })
 })();
+// 
