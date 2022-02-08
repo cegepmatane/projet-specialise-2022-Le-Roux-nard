3f85f71393b12e7dceafc0f8c3a00fc97c7d1edc
Le_Roux-nard <65075968+Le-Roux-nard@users.noreply.github.com>
2022-02-08 16:30:50 -0500

    Update #8
    # Description
    
    ##Socket.io
    Déplacement du système de websocket sur l'endpoint `/wss`
    Mise en place d'une api web sur l'endpoint `/api`
    Mise en place d'un site web sur la racine

--- poc/sockets/socket-io/client.js
+++ poc/sockets/socket-io/client.js
@@ -1,5 +1,5 @@
 const { io } = require("socket.io-client");
-const socket = io("http://localhost:8080");
+const socket = io("http://localhost:8080/wss");
 
 socket.io.on("error", (error) => {
     console.error(error);
--- poc/sockets/xmlhttp/server.js
+++ poc/sockets/xmlhttp/server.js
@@ -3,10 +3,31 @@ console.log("Lancement du seveur...");
 const express = require('express');
 const app = express();
 
+const rateLimit = require('express-rate-limit')
+const wssRateLimit = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 1, // Limit each IP to 1 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+const apiRateLimit = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 1, // Limit each IP to 1 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+const websiteRateLimit = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 1, // Limit each IP to 1 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
 const abonnes = [];
 let nouvellesDonnees = null;
 
-app.get('/register', (req, res) => {
+app.use("/wss/register", wssRateLimit);
+app.get('/wss/register', (req, res) => {
     if (!abonnes.includes(req.ip)) {
         abonnes.push(req.ip);
         res.status(200).send("OK");
@@ -15,14 +36,24 @@ app.get('/register', (req, res) => {
     }
 });
 
-app.get('/ping', (req, res) => {
+app.get('/wss/ping', (req, res) => {
     if (nouvellesDonnees) {
         res.send(nouvellesDonnees);
-        abonnes.splice(abonnes.indexOf(req.ip),1);
+        abonnes.splice(abonnes.indexOf(req.ip), 1);
     } else {
         res.status(401).send("KO");
     }
 });
+
+app.use("/api", apiRateLimit);
+app.get("/api", (req, res) => {
+    res.send("Bienvenue sur l'API!")
+})
+
+app.use("/", websiteRateLimit);
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 
 const PORT = 8080;
 app.listen(PORT, () => {
